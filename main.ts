5eee4f06b40e15dcd0d6965dda37b60d573aba4c
JITESHKUMARS <158926635+JITESHKUMARS@users.noreply.github.com>
2024-02-05 06:59:29 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 d d 2 2 2 d d 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 2 2 2 d d 2 2 2 2 . . . 
+    . . 2 2 2 2 2 2 d d 2 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+controller.player4.moveSprite(mySprite, 100, 100)
+mySprite.setImage(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 1 1 2 2 2 2 1 1 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 1 1 1 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 1 1 1 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `)
+game.reset()
+game.gameOver(true)
+game.setGameOverMessage(true, "YOU WIN ")
+game.setGameOverMessage(false, "GAME OVER!")
+game.setGameOverScoringType(game.ScoringType.HighScore)
+music.play(music.stringPlayable("- - - - - - - - ", 100), music.PlaybackMode.UntilDone)
